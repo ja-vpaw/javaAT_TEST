145a42453b3db96777b4e37161531fb65320ae29
ja-vpaw <32397674+ja-vpaw@users.noreply.github.com>
2020-04-01 18:17:53 +0300

    UI Test modify and add logs use examples

--- src/test/java/ru/WebDriverSettings.java
+++ src/test/java/ru/WebDriverSettings.java
@@ -1,15 +1,25 @@
 package ru;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
 
+import java.io.*;
+
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverSettings {
     public WebDriver chromeDriver;
+    public Logger logger;
 
     public static void son(int s){
         try {
@@ -19,32 +29,56 @@ public class WebDriverSettings {
         }
     }
 
+    @BeforeAll
+    public static void createLogFile() {
+        try {
+            File file = new File(".\\log\\chromedriver.log");
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+        } catch (java.io.IOException e) {
+        }
+    }
+
     @BeforeEach
     public void setupBellTest(){
         String osName = System.getProperty("os.name");
         System.out.println(osName);
 
+        logger=Logger.getLogger("BellTests");
+        // configure log4j properties file
+        PropertyConfigurator.configure("log4j.properties");
+
+
+
         if (osName != null && osName.contains("Linux")) {
             System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         }
         else if (osName != null && osName.contains("Windows")) {
-            System.setProperty("webdriver.chrome.driver", "C:\\geckodriver\\chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            System.setProperty("webdriver.chrome.logfile", ".\\log\\chromedriver.log");
+            //System.setProperty("webdriver.chrome.verboseLogging", "true");
         }
 
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
-        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
+        options.addArguments("--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
+        //options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
 
         chromeDriver = new ChromeDriver(options);
+        // Open browser
+        logger.info("Browser Opened");
+        // Set implicit wait
         chromeDriver.manage().window().maximize();
-        chromeDriver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+        chromeDriver.manage().timeouts().pageLoadTimeout(180, TimeUnit.SECONDS);
         chromeDriver.manage().timeouts().setScriptTimeout(40, TimeUnit.SECONDS);
         chromeDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        logger.info("Implicit wait given");
 
     }
 
     @AfterEach
     public void closeBellTest(){
+        logger.info("Test finished");
         chromeDriver.quit();
     }
+
 }
--- src/test/java/ru/open/PageOpen.java
+++ src/test/java/ru/open/PageOpen.java
@@ -2,6 +2,7 @@ package ru.open;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -13,19 +14,23 @@ public class PageOpen {
         this.chromeDriver=chromeDriver;
     }
 
-    public Float getBuyDollar() {
+    public WebElement getExchangeBlock() {
         WebDriverWait wait = new WebDriverWait(chromeDriver, 20);
         wait.until(ExpectedConditions.visibilityOf(chromeDriver.findElement(
                 By.xpath("//div[@class='main-page-exchange main-page-info__card']"))));
-        return Float.parseFloat(chromeDriver.findElement(By.xpath(
-                "(//span[contains(@class, 'main-page-exchange__rate')])[1]")).getText().replace(',', '.'));
+        return chromeDriver.findElement(By.xpath("//div[@class='main-page-exchange main-page-info__card']"));
     }
 
-    public Float getSellDollar() {
-        WebDriverWait wait = new WebDriverWait(chromeDriver, 20);
-        wait.until(ExpectedConditions.visibilityOf(chromeDriver.findElement(
-                By.xpath("//div[@class='main-page-exchange main-page-info__card']"))));
-        return Float.parseFloat(chromeDriver.findElement(By.xpath(
-                "(//span[contains(@class, 'main-page-exchange__rate')])[2]")).getText().replace(',', '.'));
+    public Float getBuyDollar(WebElement webElement) {
+        // Здесь решил оставить селектор со списком, т.к. всего два варианта
+        return Float.parseFloat(webElement.findElement
+                (By.xpath("(//tr[@class=\"main-page-exchange__row main-page-exchange__row--with-border\" and .//span/text()='USD']//span)[2]"))
+                .getText().replace(',', '.'));
+    }
+
+    public Float getSellDollar(WebElement webElement) {
+        return Float.parseFloat(webElement.findElement
+                (By.xpath("(//tr[@class=\"main-page-exchange__row main-page-exchange__row--with-border\" and .//span/text()='USD']//span)[3]"))
+                .getText().replace(',', '.'));
     }
 }
